f90ff33a2613bb7b9bd3b58a2f227d459b28f2a0
oschobel <oliver.schobel@gmail.com>
2013-01-31 15:54:28 +0100

    submit of request form is now working

--- app/Request/request_controller.rb
+++ app/Request/request_controller.rb
@@ -7,6 +7,8 @@ class RequestController < Rho::RhoController
   include BrowserHelper
   @layout = 'custom_layout'
   
+  SERVICE_HOST = Rho::RhoConfig.request_server_url
+  
   
   def request
     @products = Product.get_categories
@@ -14,6 +16,16 @@ class RequestController < Rho::RhoController
   end
   
   def submit
+    @data = "subject=Mietanfrage aus Maco-Tec App&product=#{@params['product']}&rental_begin=#{@params['rental_begin']}&operation_period=#{@params['opertaion_period']}&amount_product=#{@params['amount_product']}&location=#{@params['location']}&company=#{@params['company']}&phone=#{@params['phone']}&email=#{@params['email']}&information=#{@params['information']}"    
     
+    Rho::AsyncHttp.post(:url => SERVICE_HOST, 
+                        :headers => {"Content-Type" => "application/x-www-form-urlencoded","charset" => "UTF-8"}, 
+                        :body => @data, 
+                        :callback => url_for(:action => :after_submit),
+                        :callback_param => "post=complete")
+  end
+  
+  def after_submit
+    render '/app/index'
   end
 end
