c139b0e6e5dd0da492ceee1583a8965d0602672b
oschobel <oliver.schobel@gmail.com>
2013-02-04 13:06:57 +0100

    stuff on request submit view

--- app/Request/request_controller.rb
+++ app/Request/request_controller.rb
@@ -18,14 +18,15 @@ class RequestController < Rho::RhoController
   def submit
     @data = "subject=Mietanfrage aus Maco-Tec App&product=#{@params['product']}&rental_begin=#{@params['rental_begin']}&operation_period=#{@params['opertaion_period']}&amount_product=#{@params['amount_product']}&location=#{@params['location']}&company=#{@params['company']}&phone=#{@params['phone']}&email=#{@params['email']}&information=#{@params['information']}"    
     
-    Rho::AsyncHttp.post(:url => SERVICE_HOST, 
+    @response = Rho::AsyncHttp.post(:url => SERVICE_HOST, 
                         :headers => {"Content-Type" => "application/x-www-form-urlencoded","charset" => "UTF-8"}, 
                         :body => @data, 
                         :callback => url_for(:action => :after_submit),
                         :callback_param => "post=complete")
+    render :after_submit                  
   end
   
   def after_submit
-    render '/app/index'
+    
   end
 end
